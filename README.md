521ebb1c3706c9226deabf00de4a68786668037a
Greg Orzell <gorzell@users.noreply.github.com>
2021-03-06 10:17:24 +0000

    Update code with the correct solution and link back to the issue.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # prost-cross-package-import
+
+As noted in https://github.com/danburkert/prost/issues/440#issuecomment-785319924, the solution was to properly build out a module structure tha aligns with the protobuf package structure in our Rust code.
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,18 @@
 mod test {
-    mod v1 {
-        include!(concat!(env!("OUT_DIR"), "/test.main.v1.rs"));
+    pub(crate) mod main {
+        pub(crate) mod v1 {
+            include!(concat!(env!("OUT_DIR"), "/test.main.v1.rs"));
+        }
     }
 
-    mod other {
-        mod v1 {
+    pub(crate) mod other {
+        pub(crate) mod v1 {
             include!(concat!(env!("OUT_DIR"), "/test.other.v1.rs"));
         }
     }
 
-    mod third {
-        mod v1 {
+    pub(crate) mod third {
+        pub(crate) mod v1 {
             include!(concat!(env!("OUT_DIR"), "/test.third.v1.rs"));
         }
     }
